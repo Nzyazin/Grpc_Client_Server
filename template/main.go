9174c534c2f515c43986cb61ad5312c9c5a83702
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2022-08-23 15:29:02 +0300

    Before adding own css styles

--- template/main.go
+++ template/main.go
@@ -23,18 +23,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "index", nil)
 }
 
+func add(x, y int) int {
+	return x + y
+}
+
 func saveArticle(w http.ResponseWriter, r *http.Request) {
-	//const templ = `{{range .Items}}----------------------------
-	//ID: {{ .Id}}`
-	//t := template.Must(template.New("").Parse(templ))
-	tmpl, err := template.ParseFiles("./save_article.html", "./header.html", "./footer.html")
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-	}
+	//Adding a function name to FuncMap for searching
+	funcs := template.FuncMap{"add": add}
+	//Parses files then adds the elements of argument FuncMap to the template's function map , allocates new HTML template and wraps a call to a function returning
+	tmpl := template.Must(template.New("index1.html").Funcs(funcs).ParseFiles("./save_article.html", "./header.html", "./footer.html"))
+	//returns the first value of the name component of the query
 	title := r.FormValue("title")
+	//are calling a function Do_deal() for connect to server, send a youtube API and retrieving a data from server
 	Data := greeter_client.Do_deal(title)
-	//fmt.Fprintf(w, data.Kind)
-	//fmt.Println(Data)
+
 	if title == "" {
 		fmt.Fprintf(w, "What gonna being wrong!")
 	}
